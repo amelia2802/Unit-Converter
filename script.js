8bef70b3c5a65ba7f12a4673ba13890c63256ad2
Amelia Dutta <amelia280111dutta@gmail.com>
2022-09-27 20:43:25 +0530

    fixed decimal

--- script.js
+++ script.js
@@ -14,20 +14,20 @@ convBtn.addEventListener("click",function(){
 
 if(inputEl.value!=null){
     function convLen(){
-        let meter = inputEl.value / 3.281 ;
-        let feet = inputEl.value * 3.281; 
+        let meter = (inputEl.value / 3.281).toFixed(3) ;
+        let feet = (inputEl.value * 3.281).toFixed(3); 
         lengthEl.innerHTML+=`<p>${inputEl.value} meters = ${feet} feet | ${inputEl.value} feet = ${meter} meters </p>`
     }
     
     function convVol(){
-        let lit = inputEl.value * 3.785 ;
-        let gal = inputEl.value / 3.785; 
+        let lit = (inputEl.value * 3.785).toFixed(3) ;
+        let gal = (inputEl.value / 3.785).toFixed(3); 
         volEl.innerHTML+=`<p>${inputEl.value} liters = ${gal} gallons | ${inputEl.value} gallons = ${lit} liters </p>`
     }
     
     function convMass(){
-        let lbs = inputEl.value * 2.205 ;
-        let kilo = inputEl.value / 2.205; 
+        let lbs = (inputEl.value * 2.205).toFixed(3) ;
+        let kilo = (inputEl.value / 2.205).toFixed(3); 
         massEl.innerHTML+=`<p>${inputEl.value} kilos = ${lbs} pounds | ${inputEl.value} pounds = ${kilo} kilos </p>`
     }
 }

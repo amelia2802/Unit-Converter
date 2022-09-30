ddea6f6c3158d9cf4aac84f4c48e57087bde59d1
Amelia Dutta <amelia280111dutta@gmail.com>
2022-10-01 00:40:02 +0530

    modify js

--- script.js
+++ script.js
@@ -1,36 +1,41 @@
 const inputEl = document.getElementById("input-el");
-const lengthEl = document.getElementById("len");
-const volEl = document.getElementById("vol");
-const massEl = document.getElementById("mass");
+const lengthEl = document.getElementById("len1");
+const volEl = document.getElementById("vol1");
+const massEl = document.getElementById("mass1");
 const convBtn = document.getElementById("conv-btn")
 
 convBtn.addEventListener("click",function(){
-    convLen();
-    convVol();
-    convMass();
-    inputEl.value='';
+    if(inputEl.value!=""){
+        convLen();
+        convVol();
+        convMass();
+        inputEl.value='';
+        render();
+    }
 })
 
 
-if(inputEl.value!=null){
+
     function convLen(){
         let meter = (inputEl.value / 3.281).toFixed(3) ;
         let feet = (inputEl.value * 3.281).toFixed(3); 
-        lengthEl.innerHTML+=`<p>${inputEl.value} meters = ${feet} feet | ${inputEl.value} feet = ${meter} meters </p>`
+        lengthEl.innerHTML=`${inputEl.value} meters = ${feet} feet | ${inputEl.value} feet = ${meter} meters`
     }
     
     function convVol(){
         let lit = (inputEl.value * 3.785).toFixed(3) ;
         let gal = (inputEl.value / 3.785).toFixed(3); 
-        volEl.innerHTML+=`<p>${inputEl.value} liters = ${gal} gallons | ${inputEl.value} gallons = ${lit} liters </p>`
+        volEl.innerHTML=`${inputEl.value} liters = ${gal} gallons | ${inputEl.value} gallons = ${lit} liters`
     }
     
     function convMass(){
         let lbs = (inputEl.value * 2.205).toFixed(3) ;
         let kilo = (inputEl.value / 2.205).toFixed(3); 
-        massEl.innerHTML+=`<p>${inputEl.value} kilos = ${lbs} pounds | ${inputEl.value} pounds = ${kilo} kilos </p>`
+        massEl.innerHTML=`${inputEl.value} kilos = ${lbs} pounds | ${inputEl.value} pounds = ${kilo} kilos `
     }
-}
+
+  
+
 
 
 
